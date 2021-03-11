923f9f583314f8978ee588216f3074fbe69e61ca
fengshi96 <fengshichn@gmail.com>
2021-03-11 17:21:34 -0500

    Mar 11

---
+++ cmake-build-debug/CMakeFiles/TEST_LAPACK.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Barn/Lab/MyCppKit/main.cpp" "/Barn/Lab/MyCppKit/cmake-build-debug/CMakeFiles/TEST_LAPACK.dir/main.cpp.o"
+  "/Barn/Lab/MyCppKit/src/Helper.cpp" "/Barn/Lab/MyCppKit/cmake-build-debug/CMakeFiles/TEST_LAPACK.dir/src/Helper.cpp.o"
+  "/Barn/Lab/MyCppKit/src/Matrix.cpp" "/Barn/Lab/MyCppKit/cmake-build-debug/CMakeFiles/TEST_LAPACK.dir/src/Matrix.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/OpenBLAS/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TEST_LAPACK.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TEST_LAPACK.dir/main.cpp.o"
+  "CMakeFiles/TEST_LAPACK.dir/src/Helper.cpp.o"
+  "CMakeFiles/TEST_LAPACK.dir/src/Matrix.cpp.o"
+  "TEST_LAPACK"
+  "TEST_LAPACK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TEST_LAPACK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
